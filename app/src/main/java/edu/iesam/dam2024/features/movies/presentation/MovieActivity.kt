e67bd61539936750afe04f10a19bb0d684975a91
Alessandro Paucar <alessandroj.pau@educa.jcyl.es>
2024-09-24 19:07:49 +0200

    Search by movie structure

--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieActivity.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieActivity.kt
@@ -2,34 +2,43 @@ package edu.iesam.dam2024.features.movies.presentation
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import edu.iesam.dam2024.R
-import edu.iesam.dam2024.features.movies.data.MovieDataRepository
-import edu.iesam.dam2024.features.movies.data.remote.MovieMockRemoteDataSource
-import edu.iesam.dam2024.features.movies.domain.GetMoviesUseCase
-import edu.iesam.dam2024.features.movies.domain.MovieRepository
+import edu.iesam.dam2024.features.movies.domain.Movie
+
 
 class MovieActivity : AppCompatActivity() {
 
     private val movieFactory : MovieFactory = MovieFactory()
+    private val viewModel = movieFactory.buildViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
-        //setContentView(R.layout.activity_main)
-        val viewModel = movieFactory.buildViewModel()
+        setContentView(R.layout.activity_main)
         val movies = viewModel.viewCreated()
-        Log.d("@dev",movies.toString())
-
-        /*
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        bindData(movies)
+        viewModel.itemSelected(movies.first().id) //Simular un click sobre un item
+    }
+    private fun bindData(movies: List<Movie>){
+        findViewById<TextView>(R.id.movie_id_1).text = movies[0].id
+        findViewById<TextView>(R.id.movie_id_1).text = movies[0].title
+        findViewById<LinearLayout>(R.id.layout_1).setOnClickListener {
+                val movie1: Movie? = viewModel.itemSelected(movies[0].id)
+                movie1?.let{
+                    Log.d("@dev","Pelicula seleccionada: ${it.title}")
+                }
         }
-         */
+
+        findViewById<TextView>(R.id.movie_id_1).text = movies[1].id
+        findViewById<TextView>(R.id.movie_id_1).text = movies[1].title
+
+        findViewById<TextView>(R.id.movie_id_1).text = movies[2].id
+        findViewById<TextView>(R.id.movie_id_1).text = movies[2].title
+
+        findViewById<TextView>(R.id.movie_id_1).text = movies[3].id
+        findViewById<TextView>(R.id.movie_id_1).text = movies[3].title
+
     }
 }
