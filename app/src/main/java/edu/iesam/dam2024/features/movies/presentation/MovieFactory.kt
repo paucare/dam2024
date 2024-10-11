2587922dbc540eeb23a1e25d983cea51dbf34f61
Alessandro Paucar <146753377+paucare@users.noreply.github.com>
2024-10-11 18:47:00 +0200

    Superheroes model added (#4)
    Superhero model added

--- app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieFactory.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/presentation/MovieFactory.kt
@@ -6,18 +6,13 @@ import edu.iesam.dam2024.features.movies.data.local.MovieXmlLocalDataSource
 import edu.iesam.dam2024.features.movies.data.remote.MovieMockRemoteDataSource
 import edu.iesam.dam2024.features.movies.domain.GetMovieUseCase
 import edu.iesam.dam2024.features.movies.domain.GetMoviesUseCase
+import edu.iesam.dam2024.features.movies.domain.MovieRepository
 
-class MovieFactory (private val context: Context){
-    private val movieMockRemote = MovieMockRemoteDataSource()
-    private val movieLocal = MovieXmlLocalDataSource(context)
-    private val dataRepository = MovieDataRepository(movieMockRemote,movieLocal)
-    private val getMoviesUseCase = GetMoviesUseCase(dataRepository)
-    private val getMovieUseCase = GetMovieUseCase(dataRepository)
+class MovieFactory {
 
-    fun buildViewModel(): MoviesViewModel {
-        return MoviesViewModel(getMoviesUseCase)
-    }
-    fun buildMovieDetailViewModel(): MovieDetailViewModel {
-        return MovieDetailViewModel(getMovieUseCase)
+    fun buildViewModel(): MovieViewModel {
+        return MovieViewModel(
+        GetMoviesUseCase(MovieDataRepository(MovieMockRemoteDataSource())),
+        GetMovieUseCase(MovieDataRepository(MovieMockRemoteDataSource())))
     }
 }
