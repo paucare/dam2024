df900e3a194269956e9f3d3665440b6ad2ca16f9
ALESPAU <alessandroj.pau@educa.jcyl.es>
2024-10-14 20:45:39 +0200

    View updated

--- app/src/main/java/edu/iesam/dam2024/features/movies/data/remote/MovieMockRemoteDataSource.kt
+++ app/src/main/java/edu/iesam/dam2024/features/movies/data/remote/MovieMockRemoteDataSource.kt
@@ -13,7 +13,10 @@ class MovieMockRemoteDataSource {
         Movie("3","Mad Max: Fury Road","https://pics.filmaffinity.com/mad_max_fury_road-429261909-large.jpg"),
         Movie(title="Paddington in Peru",poster="https://pics.filmaffinity.com/paddington_in_peru-611269354-large.jpg",id="4"),
         Movie("4","Pom Poko","https://pics.filmaffinity.com/heisei_tanuki_gassen_ponpoko-405273104-large.jpg"),
-        Movie("5","The Lighthouse","https://pics.filmaffinity.com/the_lighthouse-929309607-large.jpg")
+        Movie("5","The Lighthouse","https://pics.filmaffinity.com/the_lighthouse-929309607-large.jpg"),
+        Movie("6","Fallen Angels","https://pics.filmaffinity.com/duo_luo_tian_shi_fallen_angels-941092251-large.jpg"),
+        Movie("7","Pride & Prejudice","https://pics.filmaffinity.com/pride_and_prejudice-629327318-large.jpg")
+
     )
 
     fun getMovie(movieId : String): Movie? {
